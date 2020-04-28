7ad55ee182b444e9f5f3d5fe10b385b8574cb1f0
ottyajp <ottyajp@gmail.com>
2020-04-28 11:31:17 +0900

    リロードと保存の抑制

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+window.document.onkeydown = (e) => {
+  if (
+    (e.key === 'r' && e.ctrlKey) || e.key === 'F5' // Ctrl+R, F5
+    || (e.key === 's' && e.ctrlKey) // Ctrl+S
+  ) {
+    return false;
+  }
+}
+
 // Application key
 window.oncontextmenu = () => {
   return false;
