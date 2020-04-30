8fe905b99dfcd46b2f41b449089de5d4ae6bf785
ottyajp <ottyajp@gmail.com>
2020-04-30 12:58:21 +0900

    検索したい

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ window.document.onkeydown = (e) => {
   if (
     (e.key === 'r' && e.ctrlKey) || e.key === 'F5' // Ctrl+R, F5
     || (e.key === 's' && e.ctrlKey) // Ctrl+S
+    || (e.key === 'f' && e.ctrlKey) // Ctrl+F
     || e.key === 'F10' // F10
   ) {
     return false;
